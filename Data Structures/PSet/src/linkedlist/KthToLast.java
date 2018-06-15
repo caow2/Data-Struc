fed3fcbcff267659d2b52209bed6d9f7f4d47525
caow2 <caow2@students.rowna.edu>
2018-06-14 22:34:17 -0400

    Implemented HashTable

---
+++ Data Structures/PSet/src/linkedlist/KthToLast.java
@@ -0,0 +1,73 @@
+package linkedlist;
+
+//Find kth to last element of singly linked list
+public class KthToLast {
+	public static int counter = 0;
+	public static void main(String[] args) {
+		Node<String> head = new Node<String>("First");
+		Node<String> temp = head;
+		temp.next = new Node<String>("Last - 4");
+		temp = temp.next;
+		temp.next = new Node<String>("Last - 3 ");
+		temp = temp.next;
+		temp.next = new Node<String>("Last - 2");
+		temp = temp.next;
+		temp.next = new Node<String>("Last - 1");
+		temp = temp.next;
+		temp.next = new Node<String>("Last");
+		temp = temp.next;
+		
+		System.out.println(kthToLast(head, 2));
+		System.out.println(kthToLastR(head, 2));
+	}
+	
+	/*
+	 	BCR is O(N) because need to traverse list at least once -> don't have reference to tail of list
+	 	
+	 	Brute Force -> Get length of list by traversing once. Calculate index of kth to last element
+	 	(length - 1 - k) and iterate thru list again. O(2N) ~ O(N) Time, O(1) Space
+	 	
+	 	Recursive Approach - recurse until tail, then propagate +1 back up the runtime stack.
+	 	Check the current sum from propagating each time before you propagate 1.
+	 	If sum is k, return the current element.
+	 	Assume k is within bounds - positive and less than length of entire linked list
+	 	O(N) Time, O(N) Space
+	 */
+	
+	//Brute Force
+	public static String kthToLast(Node<String> head, int k) {
+		if(head == null) 
+			return "";
+		
+		int size = 0;
+		Node<String> temp = head;
+		
+		while(temp != null) {
+			size++;
+			temp = temp.next;
+		}
+		
+		temp = head;
+		for(int i = 0; i < size - 1 - k; i++) {
+			temp = temp.next;
+		}
+		
+		return temp.value;	
+	}
+	
+	//Recursive
+	public static String kthToLastR(Node<String> head, int k) {
+		if(head == null || head.next ==  null)
+			return "";
+		
+		String value = kthToLastR(head.next, k);
+		counter++;
+		//recurse to tail
+		if(counter == k)
+			value += head.value;
+		
+		return value;
+		
+	}
+
+}
---
+++ Data Structures/PSet/src/linkedlist/RemoveDupe.java
@@ -0,0 +1,58 @@
+package linkedlist;
+
+import java.util.HashSet;
+//Remove all duplicates from unsorted linked list
+public class RemoveDupe {
+	public static void main(String[] args) {
+		// [0, 1, 2, 0, 2] -> [0, 1, 2]
+		Node<Integer> head = new Node<Integer>(0);
+		Node<Integer> temp = head;
+		temp.next = new Node<Integer>(1);
+		temp = temp.next;
+		temp.next = new Node<Integer>(2);
+		temp = temp.next;
+		temp.next = new Node<Integer>(0);
+		temp = temp.next;
+		temp.next = new Node<Integer>(2);
+		temp = temp.next;
+		
+		System.out.println(Node.toString(removeDupe(head)));
+	}
+	
+	/*
+ 	BCR is O(N) since need to check entire list from head to tail
+ 	
+ 	Keep a reference to the previous Node for removal
+ 	
+ 	Brute force - for each node, traverse thru the list from the next node onward to the tail and remove all dupes
+ 	O(N^2) Time, O(1) Space
+ 	
+ 	Cache Seen Nodes - as we traverse thru the list, add the values we've already seen to a HashSet
+ 	If in set, remove from the list. Otherwise, add to the set.
+ 	O(N) Time, O(N) Space
+ 	
+ 	Solution w/ no temp buffers aside from anything that takes O(1) space - sort the list based on values
+ 	Traverse thru the list and at each new element, have a loop that goes to the next new element and set
+ 	the elem.next to the next new element until you reach the end of the list
+ 	O(NlogN + N) ~ O(NlogN) Time, O(1) Space
+	*/
+	
+	//Cached Approach
+	public static Node<Integer> removeDupe(Node<Integer> head) {
+		if(head == null)
+			return head;
+		
+		HashSet<Integer> set = new HashSet<Integer>();
+		Node<Integer> temp = head, prev = head;
+		while(temp != null) {
+			if(set.contains(temp.value))
+				prev.next = temp.next;
+			else {
+				set.add(temp.value);
+				prev = temp;
+			}
+			temp = temp.next;
+		}
+		return head;
+	}
+}
