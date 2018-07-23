29735e0ab98a2d6c15ed54fbe6cc37c1b6151295
caow2 <caow2@students.rowna.edu>
2018-07-22 20:56:39 -0400

    Graphs

---
+++ Data Structures/DataStructures/src/graph/Graph.java
@@ -0,0 +1,58 @@
+package graph;
+
+import java.util.HashMap;
+
+public class Graph {
+	HashMap<Integer, GraphNode> nodes = new HashMap<Integer, GraphNode>();
+	
+	public Graph() {
+		
+	}
+	
+	public boolean add(int i) {
+		GraphNode node = new GraphNode(i);
+		return add(node);
+	}
+	
+	public boolean add(GraphNode node) {
+		Integer val = node.value;
+		if(nodes.containsKey(val))
+			return false; //already has that node
+		nodes.put(val, node);
+		return true;
+	}
+	
+	public GraphNode get(Integer i) {
+		return nodes.get(i);
+	}
+	
+	//Connect for undirected graph A<->B
+	public void UDConnect(int a, int b) {
+		DConnect(a,b);
+		DConnect(b,a);
+	}
+	
+	//Connect for directed graph A->B
+	public void DConnect(int a, int b) {
+		GraphNode aNode = connectHelper(a), bNode = connectHelper(b);
+		aNode.addChild(bNode);
+	}
+	
+	//Check if key exists - if not, create the node and put it in nodes
+	private GraphNode connectHelper(int i) {
+		GraphNode node = nodes.get(i);
+		if(node == null) {
+			node = new GraphNode(i);
+			add(node);
+		}
+		return node;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for(GraphNode value : nodes.values()) {
+			sb.append(value.toString() + "\n");
+		}
+		return sb.toString();
+	}
+}
--- Data Structures/DataStructures/src/graph/GraphNode.java
+++ Data Structures/DataStructures/src/graph/GraphNode.java
@@ -1,16 +1,19 @@
 package graph;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
-// Represents undirected graph: For connected nodes A and B, A is a child of B and B is a child of A
+// Represents graph: For connected nodes A and B, A is a child of B and B is a child of A
 // A <-> B
+// If directed A->B, then A has child B
 // Assume unique nodes with unique values
 public class GraphNode {
 	Integer value;
 	Map<Integer, GraphNode> children;
 	
-	public GraphNode() {
+	public GraphNode(int value) {
+		this.value = value;
 		children = new HashMap<Integer, GraphNode>();
 	}
 	
@@ -24,5 +27,16 @@ public class GraphNode {
 	
 	public boolean removeChild(GraphNode node) {
 		return children.remove(node.value, node);
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder(value + ":");
+		Iterator<Integer> it = children.keySet().iterator();
+		while(it.hasNext()) {
+			sb.append(it.next());
+			if(it.hasNext())
+				sb.append(",");
+		}
+		return sb.toString();
 	}
 }
